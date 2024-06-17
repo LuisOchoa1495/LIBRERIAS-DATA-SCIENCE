d205f3db605c4d29652fa2da5ffd2906f515afb9
LENOVO <luisochoa.1495@gmail.com>
2024-06-17 17:21:20 -0500

    17/06/2024

---
+++ NUMPY/1-crear_arrays.py
@@ -0,0 +1,14 @@
+import numpy as np
+# Array de una dimensión
+a1 = np.array([1, 2, 3])
+print(a1)
+
+# Array de dos dimensiones
+a2 = np.array([[1, 2, 3], [4, 5, 6]])
+print(a2)
+
+# Array de tres dimensiones
+a3 = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+print(a3)
+
+#
